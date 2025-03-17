cb487b48820402df6b3cab0788179638ea02d6e6
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-03-17 16:22:10 +0100

    router and pages display implementations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 /* Implémentation du router */
+
+// Les helpers pour cacher/afficher une section et colorier les liens du menu
+import { displaySection, activateLink } from './section_display.js'
+
+const router = () => {
+  const hash = window.location.hash || '#home'
+  const hashSplit = hash.split('-')
+
+  // Colorie le lien (la première partie de l'url match toujours avec un élément du menu,
+  // par choix de consistence dans le nommage)
+  activateLink(hashSplit[0])
+
+  switch(hashSplit[0]) {
+    case '#home':
+      displaySection('#home')
+    break;
+
+    case '#editions':
+      displaySection('#editions')
+    break;
+
+    case '#displays':
+      displaySection('#displays')
+    break;
+
+    case '#genres':
+      displaySection('#genres')
+    break;
+  }
+}
+
+window.addEventListener("hashchange", router)
+
+// Appelé une fois dans le vide, pour mettre à jour l'état de l'app selon l'url demandée au chargement de la page
+router()
