48c95db82cc145a00e8fe627e2c6127a5260e113
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-04-03 16:06:02 +0200

    add edition section html

--- src/sections/editions.js
+++ src/sections/editions.js
@@ -2,15 +2,6 @@ import data from '../data/editions.json';
 
 
 function displayEdition() {
-    const section = document.querySelector(`#editions`);
-
-    data.forEach(element => {
-        const p = document.createElement("p");
-        p.innerText = element.year;
-
-        section.append(p);
-
-    });
     }
 
 export { displayEdition }
